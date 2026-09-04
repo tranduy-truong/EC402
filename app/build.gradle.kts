9dd80d9a0e33c48f45856309070098f248701614
tranduy-truong <23521694@gm.uit.edu.vn>
2026-09-04 21:22:31 +0700

    build: connect NovaTech to Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -27,6 +28,7 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.03.00"))
+    implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
@@ -35,5 +37,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
